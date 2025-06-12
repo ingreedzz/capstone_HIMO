ed14273459d35e94fe53d2d643ae7cee248a8ac3
Gary Anderson Theng <107256930+ingreedzz@users.noreply.github.com>
2025-06-12 19:23:56 +0700

    Add files via upload

--- HiddenMood-main/vite.config.js
+++ HiddenMood-main/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.', // Build from the root
+  root: '.',
   build: {
-    outDir: 'dist', // Output to a dedicated dist directory
-    emptyOutDir: true, // Clean the output directory before building
-    assetsDir: 'assets', // Keep bundled assets in assets/ folder
-    assetsInclude: ['views/*.html'], // Include views folder HTML files in build
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: 'assets',
   },
 });
